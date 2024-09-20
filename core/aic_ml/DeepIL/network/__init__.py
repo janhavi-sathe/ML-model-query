09c2ece1b23452e1ec45596c2f60469746d7d7f3
Sangwon Seo <prizeworthy3@gmail.com>
2024-09-20 17:51:18 -0500

    remove old DeepIL code

--- core/aic_ml/DeepIL/network/__init__.py
+++
@@ -1,4 +0,0 @@
-from .policy import ContinousPolicy, DiscretePolicy, AbstractPolicy  # noqa: F401, E501
-from .value import StateFunction, StateActionFunction  # noqa: F401
-from .disc import GAILDiscrim  # noqa: F401
-from .transition import ContinousTransition, DiscreteTransition, AbstractTransition  # noqa: F401, E501
