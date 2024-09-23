df1c482aef454162e273d728059405b25eca0803
Sangwon Seo <prizeworthy3@gmail.com>
2024-09-23 15:12:54 -0500

    change python version

--- domains/setup.py
+++ domains/setup.py
@@ -11,7 +11,7 @@ setup(name="aic_domain",
       long_description=long_description,
       long_description_content_type="text/markdown",
       packages=find_packages(exclude=["tests", "tests.*"]),
-      python_requires='>=3.10',
+      python_requires='>=3.8',
       install_requires=[
           'numpy', 'matplotlib', 'click', 'mujoco-py<2.2,>=2.1', 'gym',
           'cython<3', 'opencv-python', 'Box2D'
