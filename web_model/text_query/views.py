15595effb20fbf6b89e6a3ac96f9f4b950c8a37a
Janhavi Sathe <32736553+janhavi-sathe@users.noreply.github.com>
2025-05-02 16:20:48 -0500

    modified for new data

--- web_model/text_query/views.py
+++ web_model/text_query/views.py
@@ -5,6 +5,8 @@ import numpy as np
 import os
 import json, csv
 
+snips_id2label = {0: 'AddToPlaylist', 1: 'BookRestaurant', 2: 'GetWeather', 3: 'PlayMusic', 4: 'RateBook', 5: 'SearchCreativeWork', 6: 'SearchScreeningEvent'}
+
 @text_query_bp.route('/check_status', methods=['GET'])
 def check_status():
     # Returns whether the table data has been loaded.
@@ -22,29 +24,30 @@ def query():
     if request.method == "POST":
         category = request.json.get("class_value")
         page = request.json.get("page", 1)  # Default is Page 1
-        per_page = 8  # Modified to display 8 texts per page
+        per_page = int(request.args.get("per_page", 8))   # Default display 8 texts per page
     else:
         category = request.args.get("class_value")
         page = int(request.args.get("page", 1))  # Default is Page 1
-        per_page = 8  # Modified to display 8 texts per page
+        per_page = int(request.args.get("per_page", 8))  # Default display 8 texts per page
 
     try:
         category = int(category)
-        if category not in range(0, 6):  # Limit 0~5
-            return jsonify({"error": "Please enter a valid class value (0 to 5)."}), 400
+        
+        if category not in range(0, 6):  # Limit 0~7
+            return jsonify({"error": "Please enter a valid class value (0 to 6)."}), 400
     except (TypeError, ValueError):
         return jsonify({"error": "The class value must be a number."}), 400
 
     try:
         # Get the file path
         base_path = os.path.dirname(os.path.abspath(__file__))
-        text_data_path = os.path.join(base_path, "..", "text_data.csv")
+        text_data_path = os.path.join(base_path, "..", "snips_text_data.csv")
 
-        # Load text_data.csv
-        text_data_df = pd.read_csv(text_data_path, index_col=0)
-
+        # Load snips_text_data.csv
+        text_data_df = pd.read_csv(text_data_path)
+        print("category name: ", snips_id2label[category], type(snips_id2label[category]))
         # Get the data that meets the conditions
-        matched_indices = text_data_df.loc[text_data_df['pred_labels'] == category, ['text', 'main_words', 'pred_labels', 'prediction']]
+        matched_indices = text_data_df.loc[text_data_df['model-assigned label'] == snips_id2label[category], :] # column ['utterance', 'explanation', 'model-assigned label', 'human-assigned label']
         
         # Query Filter
         total_results = len(matched_indices)
@@ -61,7 +64,7 @@ def query():
         from collections import OrderedDict
         formatted_results = [
             OrderedDict(
-                {"Index": int(idx), "Text": row.iloc[0], "Main Words": row.iloc[1], "Class": row.iloc[2], "Prediction": row.iloc[3]}
+                {"Index": int(idx), "Text": row.iloc[0], "Human-assigned Label": row.iloc[1] , "Model-assigned Label": row.iloc[2], "Explanation": row.iloc[3]}
             )
             for idx, row in paginated_results.iterrows()
         ]
@@ -84,7 +87,7 @@ def find_similar_images():
     try:
         text_index = request.args.get("index")
         page = int(request.args.get("page", 1))  # Default is Page 1
-        per_page = 8  # Modified to display 8 texts per page
+        per_page = int(request.args.get("per_page", 8))  # Default display 8 texts per page
 
         if text_index is None:
             return jsonify({"error": "Missing text index parameter"}), 400
@@ -93,16 +96,16 @@ def find_similar_images():
 
         # Read X_test.npy (text embeddings)
         base_path = os.path.dirname(os.path.abspath(__file__))
-        sim_mat_path = os.path.join(base_path, "..", "text_sim_matrix.npy")
+        sim_mat_path = os.path.join(base_path, "..", "snips_sim_matrix.npy")
 
         sim_mat_loaded = np.load(sim_mat_path)  
 
         # Get the file path
         base_path = os.path.dirname(os.path.abspath(__file__))
-        text_data_path = os.path.join(base_path, "..", "text_data.csv")
+        text_data_path = os.path.join(base_path, "..", "snips_text_data.csv")
 
         # Load text_data.csv
-        text_data_df = pd.read_csv(text_data_path, index_col=0)
+        text_data_df = pd.read_csv(text_data_path)
 
         if text_index < 0 or text_index >= len(text_data_df):
             return jsonify({"error": "Text index out of range"}), 404
@@ -120,7 +123,7 @@ def find_similar_images():
         #print(sorted_indices)
 
         # Filter data by similar indices
-        similar_texts = text_data_df.loc[sorted_indices, ['text', 'main_words', 'pred_labels', 'prediction']]
+        similar_texts = text_data_df.loc[sorted_indices, :] # columns=['utterance', 'human-assigned label', 'model-assigned label', 'explanation']
 
         # Query Filter
         total_results = len(similar_texts)
@@ -137,7 +140,7 @@ def find_similar_images():
         from collections import OrderedDict
         formatted_results = [
             OrderedDict(
-                {"Index": int(idx), "Text": row.iloc[0], "Main Words": row.iloc[1], "Class": row.iloc[2], "Prediction": row.iloc[3]}
+                {"Index": int(idx), "Text": row.iloc[0], "Human-assigned Label": row.iloc[1] , "Model-assigned Label": row.iloc[2], "Explanation": row.iloc[3]}
             )
             for idx, row in paginated_results.iterrows()
         ]
@@ -154,7 +157,7 @@ def find_similar_images():
     except ValueError:
         return jsonify({"error": "Invalid text index"}), 400
     except FileNotFoundError:
-        return jsonify({"error": "text_sim_matrix.npy not found"}), 500
+        return jsonify({"error": "snips_sim_matrix.npy not found"}), 500
     except Exception as e:
         return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
 
@@ -168,17 +171,17 @@ def find_keyword():
         keyword = keyword.strip()
 
         page = int(request.args.get("page", 1))  # Default is Page 1
-        per_page = 8  # Modified to display 8 texts per page
+        per_page = int(request.args.get("per_page", 8))  # Deafult display 8 texts per page
 
         # Get the file path
         base_path = os.path.dirname(os.path.abspath(__file__))
-        text_data_path = os.path.join(base_path, "..", "text_data.csv")
+        text_data_path = os.path.join(base_path, "..", "snips_text_data.csv")
 
         # Load text_data.csv
-        text_data_df = pd.read_csv(text_data_path, index_col=0)
+        text_data_df = pd.read_csv(text_data_path)
 
         # Get texts that contain the keyword
-        keyword_data = text_data_df.loc[text_data_df['text'].astype(str).str.contains(keyword, na=False), ['text', 'main_words', 'pred_labels', 'prediction']]
+        keyword_data = text_data_df.loc[text_data_df['utterance'].astype(str).str.contains(keyword, na=False), :] #columns=['utterance', 'human-assigned label', 'model-assigned label', 'explanation']
 
         # Query Filter
         total_results = len(keyword_data)
@@ -198,7 +201,7 @@ def find_keyword():
         from collections import OrderedDict
         formatted_results = [
             OrderedDict(
-                {"Index": int(idx), "Text": row.iloc[0], "Main Words": row.iloc[1], "Class": row.iloc[2], "Prediction": row.iloc[3]}
+                {"Index": int(idx), "Text": row.iloc[0], "Human-assigned Label": row.iloc[1] , "Model-assigned Label": row.iloc[2], "Explanation": row.iloc[3]}
             )
             for idx, row in paginated_results.iterrows()
         ]
@@ -215,7 +218,7 @@ def find_keyword():
     except ValueError:
         return jsonify({"error": "Invalid text index"}), 400
     except FileNotFoundError:
-        return jsonify({"error": "text_sim_matrix.npy not found"}), 500
+        return jsonify({"error": "snips_text_data.csv not found"}), 500
     except Exception as e:
         return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
     
